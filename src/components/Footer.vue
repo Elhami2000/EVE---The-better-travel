9522f882067fd2ece95dde964e836300f075b436
Eron <eb47836@ubt-uni.net>
2022-02-23 15:31:49 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -38,23 +38,23 @@
   
 </template>
 
-<script>
-    import youTube from "../assets/Icons/youTube-brands.svg";
-    import twitter from "../assets/Icons/twitter-brands.svg";
-    import instagram from "../assets/Icons/instagram-brands.svg";
-    import linkedin from "../assets/Icons/linkedin-brands.svg";
-    export default{
-        name: "footer-vue",
-        components:{
-            youTube,
-            twitter,
-            instagram,
-            linkedin,
-        },
-    };
+// <script>
+//     import youTube from "../assets/Icons/youTube-brands.svg";
+//     import twitter from "../assets/Icons/twitter-brands.svg";
+//     import instagram from "../assets/Icons/instagram-brands.svg";
+//     import linkedin from "../assets/Icons/linkedin-brands.svg";
+//     export default{
+//         name: "footer-vue",
+//         components:{
+//             youTube,
+//             twitter,
+//             instagram,
+//             linkedin,
+//         },
+//     };
 
 
-</script>
+// </script>
 
 <style lang="scss"  scoped >
 
