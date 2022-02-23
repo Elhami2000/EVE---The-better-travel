88823201fae6e728a6aa45c822144be928fa31be
Eron <eb47836@ubt-uni.net>
2022-02-23 14:47:14 +0100

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <BlogPost :post="WelcomeScreen"/>
+    <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
   </div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost.vue';
 export default {
   name: "Home",
-  components: {},
+  components: {BlogPost},
+  data (){
+    return{
+      WelcomeScreen:{
+        title:"Welcome",
+        blogPost:"Linja te reja qdo dite",
+        WelcomeScreen:true,
+        photo:"coding",
+      },
+      sampleBlogPost :[
+        {
+        title:"Title 2",
+        blogHTML:"This is another line",
+        blogCoverPhoto:"beautiful-stories",
+      },
+      {
+        tile:"Title 3",
+        blogHTML:"This another line 2",
+        blogCoverPhoto:"designer-for-everyone"
+
+      },
+      ],
+  };
+  },
 };
 </script>
