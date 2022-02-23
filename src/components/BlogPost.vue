0a8e0ac7ab7727bdbe873e82ded2d0cad7858c5c
Eron <eb47836@ubt-uni.net>
2022-02-23 15:20:13 +0100

    Update BlogPost.vue

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -6,15 +6,31 @@
               <h2 v-else>{{post.title}}</h2>
               <p v-if="post.WelcomeScreen">{{post.blogPost}}</p>
               <p class="content-preview" v-else>{{post.blogHTML}}</p>
+              <router-link class="link link-light" v-if="post.WelcomeScreen" to="#">
+                 Login/Register<Arrow class="arrow arrow-light"/>
+              </router-link>
+
+                <router-link class="link link-light" v-else to="#">
+                 View The Post<Arrow class="arrow"/>
+              </router-link>
+   
+          </div>
+          <div class="blog-photo">
+              <img v-if="post.WelcomeScreen" :src="require(`../assets/blogPhotos/ ${post.photo}.jpg`)" alt=""/>
+                <img v-else :src="require(`../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)" alt=""/>
           </div>
       </div>
   </div>
 </template>
 
 <script>
+import Arrow from "../assets/Icons/arrow-right-light.svg";
 export default {
 name:"blogPost",
 props: ["post"],
+components:{
+    Arrow,
+},
 };
 </script>
 
