c2a0e1fccbfb19165db53ebdea7af38f2ecb91b1
vesa gorani <vg47661@ubt-uni.net>
2022-02-24 21:50:05 +0100

    ffffff

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="create-post">
+    <BlogCoverPreview  v-show ="this.$store.state.blogPhotoPreview"  />
       <div class="container">
           <div :class="{invisible: !error}" class="err-message">
               <p><span>Error:</span>{{ this.errorMsg }}</p>
@@ -9,12 +10,12 @@
               <div class="upload-file">
                   <label for="blog-photo">Upload Cover Photo</label>
                   <input type="file" ref="blogPhoto" id="blog-photo" @change="fileChange" accept=".png, .jpg, .jpeg"/>
-                  <button class="preview" :class="{'button-inactive' : !this.$store.state.blogPhotoFileURL}">Preview Photo</button>
+                  <button  @click= "openPreview" class="preview" :class="{'button-inactive' : !this.$store.state.blogPhotoFileURL}">Preview Photo</button>
                   <span>File Chosen: {{ this.$store.state.blogPhotoName }}</span>
               </div>
           </div>
           <div class="editor">
-              <vue-editor :editorOptions="editorSettings" v-model="blogHTML" useCustomImageHandler />
+              <vue-editor :editorOptions="editorSettings" v-model="blogHTML" useCustomImageHandler  @image-added="imageHandler"/>
           </div>
           <div class="blog-actions">
               <button>Publish Line</button>
@@ -25,6 +26,9 @@
 </template>
 
 <script>
+import BlogCoverPreview from "../components/BlogCoverPreview";
+//import firebase from "firebase/app";
+//import "firebase/storage";
 import Quill from "quill";
 window.Quill = Quill;
 const ImageResize = require("quill-image-resize-module").default;
@@ -43,12 +47,20 @@ export default {
             },
         };
     },
+    components :{
+        BlogCoverPreview
+    },
     methods: {
         fileChange() {
             this.file = this.$refs.blogPhoto.files[0];
             const fileName = this.file.name;
             this.$store.commit("fileNameChange", fileName);
             this.$store.commit("createFileURL", URL.createObjectURL(this.file));
+        },
+
+        openPreview() {
+            this.$store.commit("openPhotoPreview");
+
         },
     },
     computed: {
