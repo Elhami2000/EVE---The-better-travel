cbdaebc8e2442952c9cdd98fc0ced0fa891d04c7
Elhami2000 <em49455@ubt-uni.net>
2022-02-26 17:13:19 +0100

    Made some stuff dynamic

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -3,9 +3,9 @@
       <div class="blog-content">
           <div>
               <h2 v-if="post.WelcomeScreen">{{post.title}}</h2>
-              <h2 v-else>{{post.title}}</h2>
+              <h2 v-else>{{post.blogTitle}}</h2>
               <p v-if="post.WelcomeScreen">{{post.blogPost}}</p>
-              <p class="content-preview" v-else>{{post.blogHTML}}</p>
+              <p class="content-preview" v-else v-html="post.blogHTML"></p>
               <router-link class="link link-light" v-if="post.WelcomeScreen" to="#">
                  Login/Register<Arrow class="arrow arrow-light"/>
               </router-link>
@@ -19,7 +19,7 @@
       </div>
           <div class="blog-photo">
               <img v-if="post.WelcomeScreen" :src="require(`../assets/blogPhotos/${post.photo}.jpg`)" alt=""/>
-                <img v-else :src="require(`../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)" alt=""/>
+                <img v-else :src="post.blogCoverPhoto" alt=""/>
           </div>
   </div>
 </template>
