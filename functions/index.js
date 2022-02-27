912f6f410e12cc45e8912824fba279b49a09f503
vesa gorani <vg47661@ubt-uni.net>
2022-02-27 19:16:36 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require("firebase-functions");
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+    return admin
+        .auth()
+        .getUserByEmail(data.email)
+        .then((user) => {
+            return admin.auth().setCustomUserClaims(user.uid , {
+                admin:true,
+            });
+        })
+        .then(() => {
+            return {
+                message: 'Succes! ${data.email} has been made an admin',
+
+            };
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
